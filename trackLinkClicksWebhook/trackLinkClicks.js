796573ea288c101e9bdc4b524faa1edb59c09955
Ben Collins <ben@benlcollins.com>
2022-07-28 19:48:21 -0400

    Create trackLinkClicks.js

---
+++ trackLinkClicksWebhook/trackLinkClicks.js
@@ -0,0 +1,27 @@
+/**
+ * doPost function webhook to track link clicks
+ */
+function doPost(e) {
+
+  // get spreadsheet
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = ss.getSheetByName('Sheet1');
+  
+  // proceed if e exists
+  if (typeof e !== 'undefined') {
+    
+    // parse webhook data to extract parameters
+    const name = e.parameter.name;
+    const section = e.parameter.section;
+    const link = e.parameter.link;
+    
+    // create new timestamp
+    const d = new Date();
+
+    // append new row of data to Sheet
+    sheet.appendRow([d, section, name, link]);
+    
+    // return blank
+    return;
+  }
+}
